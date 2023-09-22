f6426f3e3116415827ede9212eca2c779573af14
Anonymous <suportdevs>
2023-09-22 17:20:56 +0600

    designed movie list

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,9 +1,25 @@
 import "./ListItem.scss";
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from "@mui/icons-material";
 
 export default function ListItem() {
     return (
         <div className="list_item">
-            <h1>Item</h1>
+            <img src={process.env.PUBLIC_URL + 'netflix_bg.jpg'} alt="" />
+            <div className="item_content">
+                <div className="content_icons">
+                    <PlayArrow />
+                    <Add />
+                    <ThumbUpAltOutlined />
+                    <ThumbDownAltOutlined />
+                </div>
+                <div className="others_info">
+                    <span>1 Hours 20 minues 50 seconds</span>
+                    <span>+10</span>
+                </div>
+                <div className="content_text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe placeat earum molestiae maxime corrupti, obcaecati quis nostrum nihil modi!</p>
+                </div>
+            </div>
         </div>
     )
 }
